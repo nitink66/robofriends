32c8b476d83f5d64d1db1cae287a1e89baa4b69c
nitink66 <60770391+nitink66@users.noreply.github.com>
2020-03-17 22:43:04 +0530

    Updates

---
+++ precache-manifest.0fc01cf1abe60f6bfd05b902099867e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "954eff1d05bfb4f00be61dccd6025c64",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2f737bcceae9e28575ea",
+    "url": "/robofriends/static/css/main.816920d0.chunk.css"
+  },
+  {
+    "revision": "e1a452469bc71b7403ab",
+    "url": "/robofriends/static/js/2.7ab315b8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.7ab315b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f737bcceae9e28575ea",
+    "url": "/robofriends/static/js/main.a405385a.chunk.js"
+  },
+  {
+    "revision": "6a5eb8d065c05feaff0d",
+    "url": "/robofriends/static/js/runtime-main.5530ba65.js"
+  }
+]);
